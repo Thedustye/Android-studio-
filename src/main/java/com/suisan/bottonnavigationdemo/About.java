7bc8308e1b287477bbaac689de7881c2aac18d5d
Thedustye <107158168+Thedustye@users.noreply.github.com>
2022-07-07 17:06:08 +0800

    Add files via upload
    Android studio简单编写底部导航栏编写（因为本人第一次编写和上传有不会的地方请谅解）

---
+++ src/main/java/com/suisan/bottonnavigationdemo/About.java
@@ -0,0 +1,47 @@
+package com.suisan.bottonnavigationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.botton_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.about);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                switch (menuitem.getItemId()){
+                    case R.id.dashbaord:
+                        startActivity(new Intent(getApplicationContext(),DashBoard.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about:
+
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ src/main/java/com/suisan/bottonnavigationdemo/DashBoard.java
@@ -0,0 +1,47 @@
+package com.suisan.bottonnavigationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.botton_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.dashbaord);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                switch (menuitem.getItemId()){
+                    case R.id.dashbaord:
+
+                        return true;
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about:
+                        startActivity(new Intent(getApplicationContext(),About.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
